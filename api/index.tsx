0e65a6946cc7f5119445b0aa0e0bc98f8918cae4
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2024-02-06 04:30:58 +0500

    Add API endpoint and axios dependency

---
+++ api/index.tsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const development = "http://localhost:5000";
+const dev = "http://goollooper.yameenyousuf.com/api";
+const production = "http://44.202.123.121/api";
+
+const BASE_URL = `${dev}/admin`;
