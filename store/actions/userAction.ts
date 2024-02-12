36714515611b3205f750defa5d987a20eef51c00
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2024-02-12 20:45:24 +0500

    handled 401 status

--- store/actions/userAction.ts
+++ store/actions/userAction.ts
@@ -1,8 +1,12 @@
 import { User } from "@/types/type";
 
-import { SET_USER } from "./actionTypes";
+import { REMOVE_USER, SET_USER } from "./actionTypes";
 
 export const setUser = (user: User) => ({
   type: SET_USER,
   payload: user,
+});
+
+export const removeUser = () => ({
+  type: REMOVE_USER,
 });
